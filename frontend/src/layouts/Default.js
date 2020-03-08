d3b87affb57287c1aff7ec72b01f9ff798f1a1ba
Kingjmk <jameelhamdan99@yahoo.com>
2020-03-08 10:21:55 +0200

    Changes to Layout

--- frontend/src/layouts/Default.js
+++ frontend/src/layouts/Default.js
@@ -3,20 +3,12 @@ import PropTypes from "prop-types";
 import { Container, Row, Col } from "shards-react";
 
 import MainNavbar from "../components/layout/MainNavbar/MainNavbar";
-import MainSidebar from "../components/layout/MainSidebar/MainSidebar";
 import MainFooter from "../components/layout/MainFooter";
 
 const DefaultLayout = ({ children, noNavbar, noFooter }) => (
   <Container fluid>
     <Row>
-      <MainSidebar />
-      <Col
-        className="main-content p-0"
-        lg={{ size: 10, offset: 2 }}
-        md={{ size: 9, offset: 3 }}
-        sm="12"
-        tag="main"
-      >
+      <Col className="main-content p-0" lg="12" md="12" sm="12" tag="main">
         {!noNavbar && <MainNavbar />}
         {children}
         {!noFooter && <MainFooter />}
@@ -38,7 +30,7 @@ DefaultLayout.propTypes = {
 
 DefaultLayout.defaultProps = {
   noNavbar: false,
-  noFooter: false
+  noFooter: true
 };
 
 export default DefaultLayout;
